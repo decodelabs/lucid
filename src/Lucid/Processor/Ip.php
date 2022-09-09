2728949791e4ad05c60dfffafb4c81423d242e08
Tom Wright <tom@inflatablecookie.com>
2022-09-09 23:39:42 +0000

    Added Compass IP support
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ src/Lucid/Processor/Ip.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @package Lucid
+ * @license http://opensource.org/licenses/MIT
+ */
+
+declare(strict_types=1);
+
+namespace DecodeLabs\Lucid\Processor;
+
+use Brick\Math\BigInteger;
+use DecodeLabs\Compass\Ip as IpAddress;
+use DecodeLabs\Exceptional;
+use DecodeLabs\Lucid\Processor;
+use DecodeLabs\Lucid\ProcessorTrait;
+
+/**
+ * @implements Processor<IpAddress>
+ */
+class Ip implements Processor
+{
+    /**
+     * @phpstan-use ProcessorTrait<IpAddress>
+     */
+    use ProcessorTrait;
+
+    public function getOutputTypes(): array
+    {
+        return ['Compass:Ip', 'DecodeLabs\\Compass\\Ip'];
+    }
+
+    /**
+     * Convert prepared value to string or null
+     */
+    public function coerce(mixed $value): ?IpAddress
+    {
+        if (!class_exists(IpAddress::class)) {
+            throw Exceptional::ComponentUnavailable(
+                'IP validation requires decodelabs-compass package'
+            );
+        }
+
+        if ($value === null) {
+            return null;
+        }
+
+        if (
+            is_int($value) ||
+            $value instanceof BigInteger ||
+            is_string($value) ||
+            $value instanceof IpAddress
+        ) {
+            return IpAddress::parse($value);
+        }
+
+        throw Exceptional::UnexpectedValue(
+            'Could not coerce value to Compass IP',
+            null,
+            $value
+        );
+    }
+}
