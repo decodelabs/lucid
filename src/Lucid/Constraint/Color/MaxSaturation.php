7588d04c4024ec1a12b9e005454a1fdea06fb676
Tom Wright <tom@inflatablecookie.com>
2022-09-12 12:19:07 +0000

    Added Color Processor and Constraints
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ src/Lucid/Constraint/Color/MaxSaturation.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @package Lucid
+ * @license http://opensource.org/licenses/MIT
+ */
+
+declare(strict_types=1);
+
+namespace DecodeLabs\Lucid\Constraint\Color;
+
+use DecodeLabs\Lucid\Constraint;
+use DecodeLabs\Lucid\ConstraintTrait;
+use DecodeLabs\Lucid\Validate\Error;
+use DecodeLabs\Spectrum\Color;
+use Generator;
+
+/**
+ * @implements Constraint<float, Color>
+ */
+class MaxSaturation implements Constraint
+{
+    /**
+     * @phpstan-use ConstraintTrait<float, Color>
+     */
+    use ConstraintTrait;
+
+    public const OUTPUT_TYPES = [
+        'Spectrum:Color'
+    ];
+
+    protected ?float $max = null;
+
+    public function getWeight(): int
+    {
+        return 20;
+    }
+
+    public function setParameter(mixed $param): static
+    {
+        $this->max = (float)$param;
+        return $this;
+    }
+
+    public function getParameter(): mixed
+    {
+        return $this->max;
+    }
+
+    public function validate(mixed $value): Generator
+    {
+        if ($value === null) {
+            return false;
+        }
+
+        if ($value->getHslSaturation() > $this->max) {
+            yield new Error(
+                $this,
+                $value,
+                '%type% value must not have saturation greater than %maxSaturation%'
+            );
+        }
+
+        return true;
+    }
+}
---
+++ src/Lucid/Constraint/Color/MinLightness.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * @package Lucid
+ * @license http://opensource.org/licenses/MIT
+ */
+
+declare(strict_types=1);
+
+namespace DecodeLabs\Lucid\Constraint\Color;
+
+use DecodeLabs\Lucid\Constraint;
+use DecodeLabs\Lucid\ConstraintTrait;
+use DecodeLabs\Lucid\Validate\Error;
+use DecodeLabs\Spectrum\Color;
+use Generator;
+
+/**
+ * @implements Constraint<float, Color>
+ */
+class MinLightness implements Constraint
+{
+    /**
+     * @phpstan-use ConstraintTrait<float, Color>
+     */
+    use ConstraintTrait;
+
+    public const OUTPUT_TYPES = [
+        'Spectrum:Color'
+    ];
+
+    protected ?float $min = null;
+
+    public function getWeight(): int
+    {
+        return 20;
+    }
+
+    public function setParameter(mixed $param): static
+    {
+        $this->min = (float)$param;
+        return $this;
+    }
+
+    public function getParameter(): mixed
+    {
+        return $this->min;
+    }
+
+    public function validate(mixed $value): Generator
+    {
+        if ($value === null) {
+            return false;
+        }
+
+        if ($value->getHslLightness() < $this->min) {
+            yield new Error(
+                $this,
+                $value,
+                '%type% value must have lightness of at least %minLightness%'
+            );
+        }
+
+        return true;
+    }
+}
